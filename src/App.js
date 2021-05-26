de221931b4f5438a70e88c83a20716998382cace
stephaniey2022 <56015168+stephaniey2022@users.noreply.github.com>
2021-05-25 19:38:48 -0500

    Very basic ui for weather app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import logo from './logo.svg';
+import logo from './sun.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Evanston Weather</h1>
+        <img style={{width: "10%", height: "10%"}} src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Sunny
         </p>
         <a
           className="App-link"
@@ -15,7 +16,15 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          High: 76°F
+        </a>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Low: 63°F
         </a>
       </header>
     </div>
